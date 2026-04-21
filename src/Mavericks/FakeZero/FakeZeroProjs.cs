08b32530d420f70157fc6647c2b96b827e01d52c
4DR14N0807 <4drian0807@gmail.com>
2026-04-20 23:45:39 -0400

    Update FakeZeroProjs.cs

--- src/Mavericks/FakeZero/FakeZeroProjs.cs
+++ src/Mavericks/FakeZero/FakeZeroProjs.cs
@@ -48,7 +48,7 @@ public class FakeZeroBuster2Proj : Projectile {
 	) {
 		weapon = FakeZero.getWeapon();
 		damager.damage = 2;
-		damager.damage = Global.miniFlinch;
+		damager.flinch = Global.miniFlinch;
 		vel.x = 350 * xDir;
 		projId = (int)ProjIds.FakeZeroBuster2;
 		reflectable = true;
